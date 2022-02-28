b24bdc9b592456a8fa3db189cc166c5f7e58ef61
weesi <francklucasehui@gmail.com>
2022-02-28 15:08:36 +0100

    routes model schemas

--- routes/visit.py
+++ routes/visit.py
@@ -9,28 +9,28 @@ visitRoute = APIRouter()
 
 @visitRoute.post('/visit')
 async def create_visit(visit: Visit):
-    db.get_collection('visit').insert_one(dict(visit))
-    return serializeList(db.get_collection('visit').find())
+    db.get_collection('visit-route').insert_one(dict(visit))
+    return serializeList(db.get_collection('visit-route').find())
 
 @visitRoute.get('/visits')
 async def find_all_visit():
-    return visitsEntity(db.get_collection('visit').find())
+    return visitsEntity(db.get_collection('visit-route').find())
 
 
 @visitRoute.get('/visit/{id}')
 async def find_one_visit(id):
-    return serializeDict(db.get_collection('visit').find_one({"_id": ObjectId(id)}))
+    return serializeDict(db.get_collection('visit-route').find_one({"_id": ObjectId(id)}))
 
 
 @visitRoute.delete('/visit/{id}')
 async def delete_one_visit(id):
-    return serializeDict(db.get_collection('visit').find_one_and_delete({"_id": ObjectId(id)}))
+    return serializeDict(db.get_collection('visit-route').find_one_and_delete({"_id": ObjectId(id)}))
 
 
 @visitRoute.put('/visit/{id}')
 async def update_one_visit(id, visit: Visit):
     db.get_collection(
-        'visit').find_one_and_update({"_id": ObjectId(id)}, {
+        'visit-route').find_one_and_update({"_id": ObjectId(id)}, {
             "$set":dict(visit)
         })
-    return serializeDict(db.get_collection('visit').find_one({"_id": ObjectId(id)}))
+    return serializeDict(db.get_collection('visit-route').find_one({"_id": ObjectId(id)}))
